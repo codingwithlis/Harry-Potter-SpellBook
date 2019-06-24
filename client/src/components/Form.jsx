7df31205b278bb722f9378903c82296f4a69908e
Rachael Otto <rachaelotto@Rachaels-MacBook-Pro.local>
2019-06-24 16:26:06 -0700

    Add close button for form

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -2,18 +2,21 @@ import React from "react";
 
 const Form = (props) => {
   return (
-    <form onSubmit={props.handleSubmit} >
-      <label>Spell Name</label>
-      <br /> 
-      <input id="name" onChange={props.handleChange}></input>
-      <br />
-      <label>Description</label>
-      <br />
-      <input id="description" onChange={props.handleChange}></input>
-      <br />
-      <button>Submit</button>
-    </form>
-  )
+    <div>
+      <button className='toggle' onClick={props.toggleForm}>X</button>
+      <form onSubmit={props.handleSubmit}>
+        <label>Spell Name</label>
+        <br />
+        <input id="name" onChange={props.handleChange} />
+        <br />
+        <label>Description</label>
+        <br />
+        <input id="description" onChange={props.handleChange} />
+        <br />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default Form;
